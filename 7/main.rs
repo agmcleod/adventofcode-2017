4f38b8f245e1520128b6b10114f453a36d8038a5
Aaron McLeod <aaron.g.mcleod@gmail.com>
2017-12-09 00:43:08 -0500

    starting on part 2, day 7

--- 7/main.rs
+++ 7/main.rs
@@ -1,5 +1,5 @@
 extern crate read_input;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn part_one(text: &String) {
     let mut all_programs: Vec<String> = Vec::new();
@@ -37,6 +37,36 @@ fn part_one(text: &String) {
     }
 }
 
+#[derive(Debug)]
+struct Program {
+    name: String,
+    weight: usize,
+    programs: Vec<String>,
+}
+
+fn part_two(text: &String) {
+    let mut programs: HashMap<String, Program> = HashMap::new();
+    for line in text.lines() {
+        let mut words_iter = line.split(" ");
+        let program_name = words_iter.next().unwrap().to_string();
+        let weight = words_iter.next().unwrap().to_string();
+        let weight: usize = weight.replace("(", "").replace(")", "").parse().unwrap();
+
+        let mut sub_programs: Vec<String> = Vec::new();
+        if let Some(_) = words_iter.next() {
+            while let Some(sub_program) = words_iter.next() {
+                sub_programs.push(sub_program.to_string().replace(",", ""));
+            }
+        }
+
+        programs.insert(program_name.clone(), Program{ name: program_name, weight: weight, programs: sub_programs });
+    }
+
+    for (key, program) in programs {
+        println!("{:?}", program);
+    }
+}
+
 fn main() {
     let text = match read_input::read_text("7/input.txt") {
         Ok(t) => t,
@@ -44,4 +74,5 @@ fn main() {
     };
 
     part_one(&text);
+    part_two(&text);
 }
