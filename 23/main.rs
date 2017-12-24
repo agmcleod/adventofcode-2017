132f17038b1500871e808ba1a7abab9060cd7a02
Aaron McLeod <aaron.g.mcleod@gmail.com>
2017-12-23 23:04:16 -0500

    work on 23 from yesterday

---
+++ 23/main.rs
@@ -0,0 +1,123 @@
+extern crate read_input;
+
+use std::collections::HashMap;
+
+mod program;
+use program::get_register_or_value;
+
+#[derive(Debug, PartialEq)]
+enum Instruction {
+    Set(String, String),
+    Mul(String, String),
+    Sub(String, String),
+    Jump(String, String),
+}
+
+trait Program {
+    fn set(&mut self, register: &String, value: &String);
+    fn sub(&mut self, register: &String, value: &String);
+    fn mul(&mut self, register: &String, value: &String);
+    fn jump(&mut self, register: &String, value: &String);
+    fn get_current_instruction(&self) -> i64;
+}
+
+struct ProgramPartOne {
+    registers: HashMap<String, i64>,
+    current_instruction: i64,
+}
+
+impl Program for ProgramPartOne {
+    fn set(&mut self, register: &String, value: &String) {
+        self.current_instruction = program::set(&mut self.registers, register, value, self.current_instruction);
+    }
+    fn sub(&mut self, register: &String, value: &String) {
+        self.current_instruction = program::sub(&mut self.registers, register, value, self.current_instruction);
+    }
+    fn mul(&mut self, register: &String, value: &String) {
+        self.current_instruction = program::mul(&mut self.registers, register, value, self.current_instruction);
+    }
+    fn jump(&mut self, register: &String, value: &String) {
+        self.current_instruction = program::jump(&mut self.registers, register, value, self.current_instruction);
+    }
+    fn get_current_instruction(&self) -> i64 {
+        self.current_instruction
+    }
+}
+
+fn process(instructions: &Vec<Instruction>, part_two: bool) {
+    let mut program = ProgramPartOne{
+        registers: HashMap::new(),
+        current_instruction: 0,
+    };
+
+    if part_two {
+        program.registers.insert("a".to_string(), 1);
+    }
+
+    let mut multiply_counts = 0;
+    let mut h_register = 0i64;
+
+    loop {
+        let instruction = instructions.get(program.get_current_instruction() as usize).unwrap();
+        match *instruction {
+            Instruction::Set(ref register, ref value) => {
+                program.set(&register, &value);
+            },
+            Instruction::Sub(ref register, ref value) => {
+                program.sub(&register, &value);
+            },
+            Instruction::Mul(ref register, ref value) => {
+                program.mul(&register, &value);
+                multiply_counts += 1;
+            },
+            Instruction::Jump(ref condition, ref offset) => {
+                program.jump(&condition, &offset);
+            }
+        }
+
+        let h = get_register_or_value(&program.registers, &"h".to_string());
+        if part_two && h != h_register {
+            h_register = h;
+            println!("{}", h_register);
+        }
+
+        if program.get_current_instruction() < 0 || program.get_current_instruction() >= instructions.len() as i64 {
+            break
+        }
+    }
+
+    println!("{}", multiply_counts);
+    println!("{:?}", program.registers.get(&"h".to_string()));
+}
+
+fn main() {
+    let text = match read_input::read_text("23/input.txt") {
+        Ok(t) => t,
+        Err(e) => panic!("{:?}", e),
+    };
+
+    let mut instructions: Vec<Instruction> = Vec::new();
+    for line in text.lines() {
+        let mut iter = line.split(" ");
+        match iter.next().unwrap() {
+            "set" => {
+                instructions.push(Instruction::Set(iter.next().unwrap().to_string(), iter.next().unwrap().to_string()));
+            },
+            "mul" => {
+                instructions.push(Instruction::Mul(iter.next().unwrap().to_string(), iter.next().unwrap().to_string()));
+            },
+            "sub" => {
+                instructions.push(Instruction::Sub(iter.next().unwrap().to_string(), iter.next().unwrap().to_string()));
+            },
+            "jnz" => {
+                instructions.push(Instruction::Jump(iter.next().unwrap().to_string(), iter.next().unwrap().to_string()));
+            },
+            _ => {
+                panic!("Unsupported command: {:?}", line)
+            }
+        }
+    }
+
+    process(&instructions, false);
+    process(&instructions, true);
+}
---
+++ 23/program.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+pub fn get_register_or_value(registers: &HashMap<String, i64>, register: &String) -> i64 {
+    if let Some(register) = registers.get(register) {
+        return *register
+    }
+
+    if let Ok(value) = register.parse() {
+        return value
+    }
+
+    0
+}
+
+pub fn set(registers: &mut HashMap<String, i64>, register: &String, value: &String, current_instruction: i64) -> i64 {
+    let value = get_register_or_value(&registers, value);
+    registers.insert(register.clone(), value);
+    current_instruction + 1
+}
+
+pub fn mul(registers: &mut HashMap<String, i64>, register: &String, value: &String, current_instruction: i64) -> i64 {
+    let current_value = get_register_or_value(&registers, register);
+    let instruction_value = get_register_or_value(&registers, value);
+    registers.insert(register.clone(), current_value * instruction_value);
+    current_instruction + 1
+}
+
+pub fn jump(registers: &mut HashMap<String, i64>, condition: &String, offset: &String, current_instruction: i64) -> i64 {
+    if get_register_or_value(&registers, &condition) != 0 {
+        current_instruction + get_register_or_value(&registers, &offset)
+    } else {
+        current_instruction + 1
+    }
+}
+
+pub fn sub(registers: &mut HashMap<String, i64>, register: &String, value: &String, current_instruction: i64) -> i64 {
+    let current_value = get_register_or_value(&registers, register);
+    let instruction_value = get_register_or_value(&registers, value);
+    registers.insert(register.clone(), current_value - instruction_value);
+    current_instruction + 1
+}
