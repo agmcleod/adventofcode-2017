57fc2de02f9a33114f9ec911566bb9b924d04fb0
Aaron McLeod <aaron.g.mcleod@gmail.com>
2017-12-29 00:46:08 -0500

    Trying day 24, need to do more backtracking

---
+++ 24/main.rs
@@ -0,0 +1,140 @@
+extern crate read_input;
+
+use std::cmp::Ordering;
+use std::cmp::max;
+use std::fmt;
+
+#[derive(Copy, Clone)]
+struct Port {
+    pins: usize,
+    used: bool,
+}
+
+impl Port {
+    fn new(pins: usize) -> Port {
+        Port{
+            pins,
+            used: false,
+        }
+    }
+}
+
+impl PartialEq for Port {
+    fn eq(&self, other: &Port) -> bool {
+        self.pins == other.pins && !self.used && !other.used
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Component {
+    left: Port,
+    right: Port,
+    strength: usize,
+}
+
+impl Component {
+    fn can_match(&mut self, other: &mut Component) -> bool {
+        if self.left == other.left {
+            self.left.used = true;
+            other.left.used = true;
+            return true
+        } else if self.left == other.right {
+            self.left.used = true;
+            other.right.used = true;
+            return true
+        } else if self.right == other.right {
+            self.right.used = true;
+            other.right.used = true;
+            return true
+        } else if self.right == other.left {
+            self.right.used = true;
+            other.left.used = true;
+            return true
+        }
+
+        false
+    }
+
+    fn is_zero(&self) -> bool {
+        self.left.pins == 0 || self.right.pins == 0
+    }
+}
+
+impl fmt::Debug for Component {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}/{}", self.left.pins, self.right.pins)
+    }
+}
+
+fn main() {
+    let text = match read_input::read_text("24/input.txt") {
+        Ok(t) => t,
+        Err(e) => panic!("{:?}", e),
+    };
+
+    let mut components = Vec::new();
+
+    for line in text.lines() {
+        let mut iter = line.split("/");
+        let left: usize = iter.next().unwrap().parse().unwrap();
+        let right: usize = iter.next().unwrap().parse().unwrap();
+        components.push(Component{ left: Port::new(left), right: Port::new(right), strength: left + right });
+    }
+
+    components.sort_by(|a, b| {
+        if a.is_zero() && b.is_zero() {
+            Ordering::Equal
+        } else if a.is_zero() && !b.is_zero() {
+            Ordering::Less
+        } else if !a.is_zero() && b.is_zero() {
+            Ordering::Greater
+        } else {
+            b.strength.cmp(&a.strength)
+        }
+    });
+
+    let mut try_zero_index = 0;
+    let mut max_zero_component_index = 0;
+
+    for component in &components {
+        if component.is_zero() {
+            max_zero_component_index += 1;
+        } else {
+            break
+        }
+    }
+
+    let mut strength_total = 0;
+    let mut start_index = max_zero_component_index;
+
+    loop {
+        let mut bridge = vec![components.get(try_zero_index).unwrap().clone()];
+
+        let mut iter = components.iter().skip(start_index);
+
+        while let Some(component) = iter.next() {
+            let len = bridge.len();
+            let mut component_clone = component.clone();
+            let did_match = {
+                let last = bridge.get_mut(len - 1).unwrap();
+                last.can_match(&mut component_clone)
+            };
+
+            if did_match {
+                bridge.push(component_clone);
+            }
+        }
+
+        strength_total = max(strength_total, bridge.iter().fold(0, |sum, component| sum + component.strength));
+
+        try_zero_index += 1;
+        if try_zero_index >= max_zero_component_index {
+            start_index += 1;
+            if start_index == components.len() - 1 {
+                break
+            }
+        }
+    }
+
+    println!("strength: {}", strength_total);
+}
